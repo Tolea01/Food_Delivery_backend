b41bda0ce6ebb4de871f75938023f8c9c1d5acb8
Tolea01 <anatoliecurbat@gmail.com>
2023-09-17 00:22:59 +0300

    update, remove, getAll, getOne user

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Get,
+  Param,
+  Patch,
+  Delete
+} from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +21,25 @@ export class UserController {
   @UsePipes(new ValidationPipe())
   create(@Body() createUser: CreateUserDto) {
     return this.userService.create(createUser)
+  }
+
+  @Get('listUsers')
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.userService.getOneUser(Number(id))
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUser: UpdateUserDto) {
+    return this.userService.updateUser(Number(id), updateUser)
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.removeUser(Number(id))
   }
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,21 +1,24 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import * as argon2 from 'argon2';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>
   ) { }
-  async create(userData: CreateUserDto) {
+
+  async create(userData: CreateUserDto): Promise<{ message: string }> {
     const existUser = await this.userRepository.findOne({
       where: {
         username: userData.username
       },
     })
+
     if (existUser) throw new BadRequestException('This user already exist');
 
     const user = await this.userRepository.save({
@@ -24,5 +27,35 @@ export class UserService {
     })
 
     return { message: "User created successfully." }
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async getOneUser(id: number): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async updateUser(id: number, updateUser: UpdateUserDto): Promise<{ message: string }> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) throw new NotFoundException('User not found!');
+
+    const updateResult = await this.userRepository.update(id, updateUser);
+
+    return updateResult.affected > 0 ? { message: 'User updated successfully' } :
+      { message: 'User not updated' };
+  }
+
+  async removeUser(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) throw new NotFoundException('User not found!');
+
+    const removeResult = await this.userRepository.delete(id);
+
+    return removeResult.affected > 0 ? { message: 'User deleted successfully' } :
+      { message: 'User not deleted' }
   }
 }
