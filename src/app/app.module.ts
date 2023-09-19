6bf3ddbabf68c107b0e67fd9f096b4cdc13265b4
Tolea01 <anatoliecurbat@gmail.com>
2023-09-19 19:24:16 +0300

    create a database config file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/dataBase.config';
 
 @Module({
   imports: [
@@ -9,16 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        synchronize: true,
-        entities: [__dirname + '/**/*.entity{.js, .ts}']
-      }),
+      useFactory: databaseConfig,
       inject: [ConfigService],
     })
   ],
---
+++ src/app/config/database.config.ts
@@ -0,0 +1,12 @@
+import { ConfigService } from "@nestjs/config";
+
+export const databaseConfig = async (configService: ConfigService): Promise<any> => ({
+  type: 'postgres',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_NAME'),
+  synchronize: true,
+  entities: [__dirname + '/../config/../**/*.entity{.js, .ts}']
+}) 
