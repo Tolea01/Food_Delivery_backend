c3613e908a2ad91d71f87e812af4a96e98df021c
Tolea01 <anatoliecurbat@gmail.com>
2023-09-28 21:20:40 +0300

    add and use column-helper function

---
+++ src/app/helpers/column-helpers.ts
@@ -0,0 +1,9 @@
+import { ColumnOptions } from "typeorm";
+
+export function createUniqueColumnOptions(length?: number): ColumnOptions {
+  return {
+    nullable: false,
+    unique: true,
+    length,
+  }
+}
--- src/app/modules/user/user.entity.ts
+++ src/app/modules/user/user.entity.ts
@@ -1,15 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
 import { UserRole } from './user-role.enum';
+import { createUniqueColumnOptions } from 'src/app/helpers/column-helpers';
+
 @Entity()
 @Unique(['username'])
 export class User {
   @PrimaryGeneratedColumn()
   id: number
 
-  @Column({ length: 50, nullable: false })
+  @Column(createUniqueColumnOptions(50))
   username: string
 
-  @Column({ nullable: false })
+  @Column(createUniqueColumnOptions())
   password: string
 
   @Column({ type: 'enum', enum: UserRole, default: UserRole.Admin})
