db4ce87e31d32953de3044a9428d911877832143
Tolea01 <anatoliecurbat@gmail.com>
2023-10-16 23:05:00 +0300

    code review

--- src/app/modules/order_co_customer/dto/update-customer.dto.ts
+++ src/app/modules/order_co_customer/dto/update-customer.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class UpdateCustomerDto {
   @IsNotEmpty()
-  @Length(3, 30, { message: "Name must be between 3 and 30 characters!" })
+  @Length(3, 30)
   @IsString()
   name?: string;
 }
