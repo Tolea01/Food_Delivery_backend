7e6ac7c076bcdfce328b4fd45b8583a5c40cc7d0
Tolea01 <anatoliecurbat@gmail.com>
2023-10-17 19:08:26 +0300

    rename order-co-customer imports

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,7 +1,7 @@
 import { AuthModule } from "./auth/auth.module";
 import { CountryModule } from "./geo/country/country.module";
 import { LocationModule } from "./geo/location/location.module";
-import { OrderCoCustomerModule } from "./order_co_customer/order-co-customer.module";
+import { OrderCoCustomerModule } from "./order-co-customer/order-co-customer.module";
 import { RegionModule } from "./geo/region/region.module";
 import { UserModule } from "./user/user.module";
 
